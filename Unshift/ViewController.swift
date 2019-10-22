5dba8ab1738bf9fc744e7ec7b6b279a6359f2efb
Dmitrii Trofimov <trofman0@gmail.com>
2019-10-22 13:01:29 +0300

    Initial commit

---
+++ Unshift/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Unshift
+//
+//  Created by Dmitrii Trofimov on 2019-10-22.
+//  Copyright © 2019 Dmitrii Trofimov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
